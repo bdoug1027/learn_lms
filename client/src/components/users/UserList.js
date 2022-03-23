0a435b9f65b92606cca081405400d0aac4796a70
Boston Douglas <bostondouglas@Bostons-MacBook-Pro.local>
2022-03-22 18:16:09 -0600

    course add

--- client/src/components/users/UserList.js
+++ client/src/components/users/UserList.js
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import { ListGroup, Button } from 'react-bootstrap';
+
 const UserList = ({ users }) => (
   <>
     <ListGroup>
       { users.map( u => 
-        <ListGroup.Item>
+        <ListGroup.Item key={u.id}>
           {u.first_name} {u.last_name}
-          <Button variant="info">Show</Button>
+          <Link to={`/users/${u.id}`}>
+            <Button variant="info">Show</Button>
+          </Link>
         </ListGroup.Item>
       )}
     </ListGroup>
